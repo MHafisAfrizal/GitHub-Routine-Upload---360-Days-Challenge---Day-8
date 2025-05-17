6782cb9cc5bac402eccc1f2ed490d35b23341b92
M. HAFIS AFRIZAL <118447793+MHafisAfrizal@users.noreply.github.com>
2025-05-17 17:34:51 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Day 8 script (optional behavior)
+document.querySelectorAll('.card button').forEach(button => {
+  button.addEventListener('click', () => {
+    alert('Terima kasih telah memilih paket kami!');
+  });
+});
